5b23185a88a8a8455340dd8521e153b35c40513b
luckyreo-crypto <luckyreo@gmail.com>
2026-07-11 20:17:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
-import requests
+import requests  
 import base64
 import datetime
 import uuid
